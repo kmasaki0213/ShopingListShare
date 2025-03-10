1d4d4c45e7571f97697c288c06fbd51b36a3797d
yomoshiro <61180346+yomoshiro@users.noreply.github.com>
2025-03-10 21:52:59 +0900

    Add owned_teams association to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,8 @@ class User < ApplicationRecord
   # ユーザーとチームの関係（多対多）
   has_many :memberships
   has_many :teams, through: :memberships
+
+  has_many :owned_teams, class_name: "Team", foreign_key: "owner_id", dependent: :destroy
+
+  has_many :items
 end
